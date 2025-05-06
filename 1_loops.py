bffb120dd0f9a327115ffb1778f589db800da14f
GG-00000 <ggutierrez48@cps.edu>
2025-05-06 12:01:35 -0500

    loop

--- 1_loops.py
+++ 1_loops.py
@@ -2,13 +2,18 @@
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
 
+
 # Challenge:
 # Use a for loop to print each fruit on a new line.
-
-
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+for fruit in fruits:
+    print(subject)
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
-
+for subject in subjects: 
+    print(subject)
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
